148dfdfa182b88a5ab7089ef1e2cd976c832fc8c
MARK2273 <ayush.mehta.2024.training@gmail.com>
2024-04-07 16:32:32 +0530

    f6 completed

--- app/config/mysql_connection.js
+++ app/config/mysql_connection.js
@@ -1,10 +1,12 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: process.env.DATABASE
-}).promise()
+const connection = mysql
+  .createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "all_tasks0",
+  })
+  .promise();
 
 module.exports = connection;
